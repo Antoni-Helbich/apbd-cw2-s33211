d3a3cd0c536a1e796e7c0ef9e4b81f7c1ef2fda4
Antoni Helbich <antoni.helbich@gmail.com>
2026-03-20 12:53:06 +0100

    Dodano strukturę klasy Wypozyczenie

--- apbd-cw2-s33211/apbd-cw2-s33211/Wypozyczenie.cs
+++ apbd-cw2-s33211/apbd-cw2-s33211/Wypozyczenie.cs
@@ -1,6 +1,21 @@
-﻿namespace apbd_cw2_s33211;
+﻿using apbd_cw2_s33211.Sprzety;
+
+namespace apbd_cw2_s33211;
 
 public class Wypozyczenie
 {
+    public Uzytkownik Uzytkownik { get; set; }
+    public Sprzet Sprzet { get; set; }
+    public DateTime DataWypozyczenia { get; set; }
+    public DateTime DataZwrotu { get; set; }
+    public bool? CzyZwrotTerminowy { get; set; }
+
+    public Wypozyczenie(Uzytkownik uzytkownik, Sprzet sprzet, DateTime dataWypozyczenia)
+    {
+        this.Uzytkownik = uzytkownik;
+        this.Sprzet = sprzet;
+        this.DataWypozyczenia = dataWypozyczenia;
+        CzyZwrotTerminowy = null;
+    }
     
 }
