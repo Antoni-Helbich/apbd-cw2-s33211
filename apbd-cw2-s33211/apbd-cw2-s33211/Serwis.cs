26291f01de71fd1da3ca2a43aba9acb17d71dfe4
Antoni Helbich <antoni.helbich@gmail.com>
2026-03-20 12:59:57 +0100

    Dodano pola i metody dodania uzytkownika i sprzetu do klasy Serwis

--- apbd-cw2-s33211/apbd-cw2-s33211/Serwis.cs
+++ apbd-cw2-s33211/apbd-cw2-s33211/Serwis.cs
@@ -1,6 +1,26 @@
-﻿namespace apbd_cw2_s33211;
+﻿using apbd_cw2_s33211.Sprzety;
+namespace apbd_cw2_s33211;
 
 public class Serwis
 {
+    List<Uzytkownik> Uzytkownicy {get; set;}
+    List<Sprzet> Sprzety {get; set;}
+
+    public Serwis()
+    {
+        Uzytkownicy = new List<Uzytkownik>();
+        Sprzety = new List<Sprzet>();
+    }
+
+    public void DodajUzytkownika(Uzytkownik uzytkownik)
+    {
+        Uzytkownicy.Add(uzytkownik);
+    }
+
+    public void DodajSprzet(Sprzet sprzet)
+    {
+        Sprzety.Add(sprzet);
+    }
+    
     
 }
